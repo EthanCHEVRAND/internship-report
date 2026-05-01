c43870f21c23a2b5866d402495a479b5b6101e53
EthanCHEVRAND <EthanCHEVRAND>
2026-05-01 11:29:03 +0200

    vue project init

---
+++ src/components/MainNav.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>
+        <div id="header-info">
+            <span class="header-info-item" id="fname">Ethan</span>
+            <span class="header-info-item" id="lname">CHEVRAND</span>
+            <span class="header-info-item" id="mail">ethan.chevrand@edu.univ-fcomte.fr</span>
+            <span class="header-info-item" id="cerp">CERP RRM</span>
+        </div>
+
+        <div id="header-nav">
+            <RouterLink to="/">Accueil</RouterLink>
+            <RouterLink to="/technique">Technique</RouterLink>
+            <RouterLink to="/suivi">Suivi de project</RouterLink>
+            <RouterLink to="/integration">Intégration</RouterLink>
+        </div>
+    </header>
+</template>
---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import TechniqueView from '../views/TechniqueView.vue'
+import SuiviView from '../views/SuiviView.vue'
+import IntegrationView from '../views/IntegrationView'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/technique',
+      name: 'technique',
+      component: TechniqueView,
+    },
+    {
+      path: '/suivi',
+      name: 'suivi',
+      component: SuiviView,
+    },
+    {
+      path: '/integration',
+      name: 'integration',
+      component: IntegrationView,
+    }
+  ],
+})
+
+export default router
